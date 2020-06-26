2b62dd113dd0fafc944cce8d945e7807e7fbe7ef
Anonymoush4cker7 <65849213+Anonymoush4cker7@users.noreply.github.com>
2020-06-26 05:48:06 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,42 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$reset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$reset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$reset"
+sleep 2.0
+clear 
+echo -e "$red                         ChEcKiNg.....>$reset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the Wordlist-G please wait$reset"
+sleep 2.0
+cd $HOME
+rm -rf Wordlist-G
+git clone https://github.com/Anonymoush4cker7/Wordlist-G
+clear
+echo " "
+echo -e "$grn               Wordlist-G has been updated check it now...$reset"
+sleep 3.0
